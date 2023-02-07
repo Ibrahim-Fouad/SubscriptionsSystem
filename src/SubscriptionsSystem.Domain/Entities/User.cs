2f83516e042b795ca18645e3a491654b71c8bcd5
Ibrahim Fouad <iibrahim.fouad@gmail.com>
2023-02-07 20:41:18 +0200

    Add user entity.

---
+++ src/SubscriptionsSystem.Domain/Entities/User.cs
@@ -0,0 +1,22 @@
+﻿namespace SubscriptionsSystem.Domain.Entities;
+
+public class User : Entity
+{
+    public string Name { get; private set; }
+    public string PasswordHash { get; private set; }
+    public string PasswordSalt { get; private set; }
+    public DateTime CreatedAtUtc { get; private set; }
+
+    private User(int id, string name, string passwordHash, string passwordSalt, DateTime? createdAtUtc) : base(id)
+    {
+        Name = name;
+        PasswordHash = passwordHash;
+        PasswordSalt = passwordSalt;
+        CreatedAtUtc = createdAtUtc ?? DateTime.UtcNow;
+    }
+
+    public static User Create(int id, string name, string passwordHash, string passwordSalt, DateTime? createdAtUtc)
+    {
+        return new User(id, name, passwordHash, passwordSalt, createdAtUtc);
+    }
+}
